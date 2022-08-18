e7f29033e894ffa76b2c80ccede606b9421af8f8
Devsstudent <devsstudent777@gmail.com>
2022-08-18 19:32:49 +0200

    updated expand in normal && bonus

--- bonus_dir/src/expand.c
+++ bonus_dir/src/expand.c
@@ -3,83 +3,150 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mbelrhaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/03 18:15:06 by odessein          #+#    #+#             */
-/*   Updated: 2022/08/17 17:26:58 by odessein         ###   ########.fr       */
+/*   Created: 2022/08/17 17:58:23 by mbelrhaz          #+#    #+#             */
+/*   Updated: 2022/08/18 18:06:37 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
-//Gerer le cas particulier du "" et '' <- a remplacer par zeroooooo 0
-//$?
-//expansion dans le here_doc, ne pas gerer, ca change en fonction du nb de dollars, paire ou impaire, ne pas s'embeter avec ca
-
-void	expand(t_line *line, t_dict *dict)
-{
-	t_block	*buff;
-
-	buff = line->head;
-	while (buff)
-	{
-		if (buff->token == CMD_ARG || buff->token == FILES)
-			check_dollar_in_block(buff, dict);
-		buff = buff->next;
-	}
-}
-
-int	size_doll_val(char *word, int *i)
+int	total_char_to_remove(char **key_arr)
 {
 	int	size;
+	int	i;
 
+	i = 0;
 	size = 0;
-	(*i)++;
-	while (word[*i] && word[*i] != ' ' && word[*i] != '\'' && word[*i] != '\"'
-			&& word[*i] != '$')
+	while (key_arr[i])
 	{
-		size++;
-		(*i)++;
+		// + 1 for $
+		size += ft_strlen(key_arr[i]) + 1;
+		i++; } return (size);
+}
+
+int	total_char_to_add(char **val_arr)
+{
+	int	size;
+	int	i;
+
+	i = 0;
+	size = 0;
+	while (val_arr[i])
+	{
+		size += ft_strlen(val_arr[i]);
+		i++;
 	}
 	return (size);
 }
 
-void	fill_key_arr(t_block *block, char **key_arr)
+t_bool	check_char(char c)
+{
+	if (c > -1 && c < 48)
+		return (FALSE);
+	return (TRUE);
+}
+
+int	size_doll_val(char *word,int i)
+{
+	int	size;
+
+	size = 0;
+	i++;
+	if (word[i] >= '0' && word[i] <= '9')
+	{
+		size++;
+		return (size);
+	}
+	while (word[i] && check_char(word[i]))
+	{
+		size++;
+		i++;
+	}
+	return (size);
+}
+
+
+void	advance_if_in_s_quote(t_bool *d_quote, char *word, int *i)
+{
+	char	c;
+
+	c = word[*i];
+	if (!(*d_quote) && c == '\"')
+		*d_quote = TRUE;
+	else if (*d_quote && c == '\"')
+		*d_quote = FALSE;
+	if (!(*d_quote) && c == '\'')
+	{
+		(*i) += 1;
+		while (word[*i] && word[*i] != '\'')
+			(*i) += 1;
+	}
+}
+
+void	fill_key_arr(t_block *block, char **key_arr, int *indexes)
+{
+	int	i;
+	int	j;
+	int	size_val;
+
+	i = 0;
+	j = 0;
+	while (indexes[i] != -1)
+	{
+		size_val = size_doll_val(block->word, indexes[i]);
+		key_arr[j] = ft_substr(block->word, indexes[i] + 1, size_val);
+		ft_printf("key = %s\n", key_arr[j]);
+		//KEYS OK
+		j++;
+		i++;
+	}
+	key_arr[j] = NULL;
+}
+
+int	*get_indexes_expandables(t_block *block, int dollar)
 {
 	int		i;
 	int		j;
-	int		buff;
+	int		*indexes;
 	t_bool	d_quote;
 
 	i = 0;
 	j = 0;
+	indexes = malloc(sizeof(int) * (dollar + 1));
+	if (!indexes)
+		return (NULL);
 	d_quote = FALSE;
 	while (block->word[i])
 	{
-		if (d_quote && block->word[i] == '\"')
-			d_quote = FALSE;
-		else if (!d_quote && block->word[i] == '\"')
-			d_quote = TRUE;
-		if (!d_quote && block->word[i] == '\'')
-		{
-			i++;
-			while (block->word[i] && block->word[i] != '\'')
-				i++;
-			if (!block->word[i])
-				break ;
-		}
+		advance_if_in_s_quote(&d_quote, block->word, &i);
 		if (block->word[i + 1] && block->word[i] == '$' 
-			&& block->word[i + 1] != '$' && block->word[i + 1] != ' ' 
-			&& block->word[i + 1] != '\'' && block->word[i + 1] != '\"')
-		{
-			buff = i + 1;
-			key_arr[j] = ft_substr(block->word, buff, size_doll_val(block->word, &i));
-			ft_printf("\nkey arr : %s", key_arr[j]);
-			j++;
-		}
-		else
-			i++;
+				&& check_char(block->word[i + 1]))
+			indexes[j++] = i;
+		i++;
 	}
-	key_arr[j] = NULL;
+	indexes[j] = -1;
+	return (indexes);
+}
+
+int	get_nb_of_dollar(t_block *block)
+{
+	int		i;
+	int		dollar;
+	t_bool	d_quote;
+
+	i = 0;
+	dollar = 0;
+	d_quote = FALSE;
+	while (block->word[i])
+	{
+		advance_if_in_s_quote(&d_quote, block->word, &i);
+		if (block->word[i] == '$' && block->word[i + 1] 
+				&& check_char(block->word[i + 1]))
+			dollar++;
+		i++;
+	}
+	return (dollar);
 }
 
 void	fill_val_arr(char **key_arr, char **val_arr, t_dict *dict)
@@ -99,41 +166,12 @@ void	fill_val_arr(char **key_arr, char **val_arr, t_dict *dict)
 			if (!value)
 				free_exit();
 			val_arr[i] = value;
+			//ft_printf("value = %s\n", val_arr[i]);
+			//values OK
 		}
 		i++;
 	}
 	val_arr[i] = NULL;
-}
-
-int	total_char_to_remove(char **key_arr)
-{
-	int	size;
-	int	i;
-
-	i = 0;
-	size = 0;
-	while (key_arr[i])
-	{
-		// + 1 for $
-		size += ft_strlen(key_arr[i]) + 1;
-		i++;
-	}
-	return (size);
-}
-
-int	total_char_to_add(char **val_arr)
-{
-	int	size;
-	int	i;
-
-	i = 0;
-	size = 0;
-	while (val_arr[i])
-	{
-		size += ft_strlen(val_arr[i]);
-		i++;
-	}
-	return (size);
 }
 
 void	replace_key(char *new_word, int *j, char **val_arr)
@@ -151,126 +189,134 @@ void	replace_key(char *new_word, int *j, char **val_arr)
 	}
 }
 
-void	fill_new_word(char *new_word, char *word, char **val_arr)
+t_bool	handle_simple_word(char *new_word, char *word, int *indexes)
+{
+	if (indexes[0] == -1)
+	{
+		ft_strlcpy(new_word, word, ft_strlen(word) + 1);
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+t_bool	char_is_num(char c, int *i)
+{
+	if (c >= '0' && c <= '9')
+	{
+		(*i)++;
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+void	init_i_j_k(int *i, int *j, int *k)
+{
+	*i = 0;
+	*j = 0;
+	*k = 0;
+}
+
+void	fill_new_word(char *new_word, char *word, char **val_arr, int *indexes)
 {
 	int		i;
 	int		j;
 	int		k;
-	t_bool	quote;
-	t_bool	d_quote;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	quote = FALSE;
-	d_quote = FALSE;
+	init_i_j_k(&i, &j, &k);
+	if (handle_simple_word(new_word, word, indexes))
+		return ;
 	while (word[i])
 	{
-		if (word[i] == '\"' && !d_quote && !quote)
-			d_quote = TRUE;
-		else if (word[i] == '\"' && d_quote && !quote)
-			d_quote = FALSE;
-		else if (word[i] == '\'' && !quote && !d_quote)
-			quote = TRUE;
-		else if (word[i] == '\'' && quote && !d_quote)
-			quote = FALSE;
-		if (word[i] == '$' && !quote && (word[i + 1] && word[i + 1] != '$'
-			&& word[i + 1] != ' ' && word[i + 1] != '\"' && word[i + 1] != '\''))
+		while (i != indexes[k] && word[i])
+			new_word[j++] = word[i++];
+		if (word[i] && i == indexes[k])
 		{
 			i++;
-			while ((word[i] && word[i] != '$' && word[i] != ' ' && word[i] != '\"' && word[i] != '\''))
+			while (word[i] && check_char(word[i]))
+			{
+				if (word[i - 1] && word[i - 1] == '$' && char_is_num(word[i], &i))
+					break ;
 				i++;
+			}
 			replace_key(new_word, &j, &val_arr[k++]);
 		}
-		if  (!quote && word[i] && word[i + 1] && (word[i] == '$' || word[i] == '\'') && word[i + 1] != ' ' && word[i + 1] != '\"' && word[i + 1] != '\'' && word[i + 1] != '$')
-			continue ;
-		ft_printf("\nnot $ : %c", word[i]);
-		new_word[j] = word[i];
-		if (!word[i])
-			return ;
-		i++;
-		j++;
 	}
+	new_word[j] = '\0';
 }
 
-void	expand_block(t_block *block, char **key_arr, char **val_arr)
+void	expand_block(t_block *block, char **key_arr, char **val_arr, int *indexes)
 {
-	//function total_char_to_remove key
-	//funtion	total_char_to_add val all_char_statying
-	//function all_char_staying
 	int		size;
 	char	*new_word;
 
 	size = ft_strlen(block->word) + total_char_to_add(val_arr) - total_char_to_remove(key_arr);
-	new_word = (char *)malloc(sizeof(*new_word) * (size + 1));
+	//ft_printf("SIZE = %i\n", size);
+	//SIZE OK
+	new_word = (char *) malloc(sizeof(*new_word) * (size + 1));
 	if (!new_word)
 		free_exit();
-	fill_new_word(new_word, block->word, val_arr);
-	//ft_printf("val_arr[0] = %s\n", val_arr[0]);
-	ft_printf("new word: %s\n", new_word);
+	fill_new_word(new_word, block->word, val_arr, indexes);
+	ft_printf("new_word: %s\n", new_word);
 	add_to_gc(SIMPLE, block->word, get_gc());
 	block->word = new_word;
-	//browse the string and replace
 }
 
-void	check_dollar_in_block(t_block *block, t_dict *dict)
+void	handle_dollar_in_block(t_block *block, t_dict *dict)
 {
 	char	**key_arr;
 	char	**val_arr;
 	int		size_double_arr;
+	int		*indexes;
 
 	size_double_arr = get_nb_of_dollar(block);
+	ft_printf("***nb expandable = %i***\n", size_double_arr);
+	indexes = get_indexes_expandables(block, size_double_arr);
+	if (!indexes)
+		free_exit();
+	add_to_gc(SIMPLE, indexes, get_gc());
 	key_arr = (char **) malloc(sizeof(*key_arr) * (size_double_arr + 1));
 	if (!key_arr)
 		free_exit();
 	add_to_gc(DOUBLE, key_arr, get_gc());
-	fill_key_arr(block, key_arr);
-//	ft_printf("key_arr[0] = %s\n", key_arr[0]);
+	fill_key_arr(block, key_arr, indexes);
 	val_arr = (char **) malloc(sizeof(*val_arr) * (size_double_arr + 1));
 	if (!val_arr)
 		free_exit();
 	add_to_gc(DOUBLE, val_arr, get_gc());
 	fill_val_arr(key_arr, val_arr, dict);
-	//ft_printf("TETSTSET : %s\n", val_arr[0]);
-	expand_block(block, key_arr, val_arr);
+	expand_block(block, key_arr, val_arr, indexes);
 }
 
-int	get_nb_of_dollar(t_block *block)
+void	expand(t_line *line, t_dict *env)
 {
-	int		i;
-	int		dollar;
-	t_bool	d_quote;
+	t_block	*buff;
 
-	i = 0;
-	dollar = 0;
-	d_quote = FALSE;
-	while (block->word[i])
+	buff = line->head;
+	while (buff)
 	{
-		if (!d_quote && block->word[i] == '\"')
-			d_quote = TRUE;
-		else if (d_quote && block->word[i] == '\"')
-			d_quote = FALSE;
-		if (!d_quote && block->word[i] == '\'')
-		{
-			i++;
-			while (block->word[i] && block->word[i] != '\'')
-				i++;
-		}
-		if (block->word[i + 1] && block->word[i] == '$' 
-				&& block->word[i + 1] != '$' && block->word[i + 1] != ' ' 
-				&& block->word[i + 1] != '\'' && block->word[i + 1] != '\"')
-			dollar++;
-		i++;
+		if (buff->token == CMD_ARG || buff->token == FILES)
+			handle_dollar_in_block(buff, env);
+		buff = buff->next;
 	}
-	ft_printf("\ndollar : %i", dollar);
-	return (dollar);
 }
 
-//search for value in our dictionary
-//get the value, strlen, and put it in our string
+void	browse_ast_apply_expand(t_leaf *leaf, t_dict *env)
+{
+	t_line *line;
 
-//Function qui va checker dans l'env si la key exist
-//Si la key n'existe pas on suprimme le block; 
-//(si il n'y a pas de quote sinon le block est egal a 0);
-
-//Else if la value existe on remplace le block
+	if (!leaf || !env)
+		return ;
+	if (leaf->type == CMD)
+	{
+		line = leaf->content;
+		expand(line, env);
+	}
+	if (leaf->left != NULL)
+		browse_ast_apply_expand(leaf->left, env);
+	else
+		return ;
+	if (leaf->right != NULL)
+		browse_ast_apply_expand(leaf->right, env);
+	else
+		return ;
+}
