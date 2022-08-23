0050421480c6ceaba9051a286b842526e05874bf
Maha BELRHAZI <mbelrhaz@e1r12p16.clusters.42paris.fr>
2022-08-23 18:32:39 +0200

    added env and unset builtins

---
+++ builtins/env.dir/env.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelrhaz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 17:38:52 by mbelrhaz          #+#    #+#             */
+/*   Updated: 2022/08/23 17:49:50 by mbelrhaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "minishell.h"
+
+int	exec_env(int ac, char **argv, t_dict *env)
+{
+	int		i;
+	char	**double_arr_env;
+
+	(void)ac;
+	(void)argv;
+	i = 0;
+	double_arr_env = dict_to_double_char(env);
+	while (double_arr_env[i])
+	{
+		ft_putstr_fd(double_arr_env[i], 1);
+		write(1, "\n", 1);
+		i++;
+	}
+	errno = 0;
+}
