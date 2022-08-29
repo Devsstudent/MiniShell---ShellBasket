937b8ecfd0002ed7abfb3d40430964bd121a4ca2
Devsstudent <devsstudent777@gmail.com>
2022-08-29 13:06:12 +0200

    heredoc to finish

--- src/here_doc.c
+++ src/here_doc.c
@@ -1,6 +1,8 @@
 #include "minishell.h"
 
-//Cree un tmp file pour chaque block here_doc
+//Cree un tmp file pour chaque block qui contient des here_doc
+
+//Malloc un tableau de fd pour chaque line avec le nombre de sub_line
 
 //Si plusieurs heredoc on ecrit dans le premier puis le deuxieme etc
 
@@ -12,19 +14,40 @@
 
 //Ensuite on close tous les tmp file dans la free_each_turn
 
-void	parse_here_doc(t_leaf *leaf)
+//a Checker
+int	total_block(t_leaf *leaf)
+{
+	int	i;
+
+	i = 0;
+	if (leaf->type == CMD)
+		return (1);
+	else if (leaf->type = PIPE_L)
+	{
+		while (leaf != NULL)
+		{
+			i++;
+			leaf = leaf->right;
+		}
+		return (i);
+	}
+}
+
+fd_arr = malloc(sizeof(*fd_arr) * total_block(tree->head) + 1); ou pas
+
+void	parse_here_doc(t_leaf *leaf, int *fd_arr, int turn)
 {
 	if (leaf->type == PIPE_L)
 	{
-		check_here_doc(leaf->left->content);
+		check_here_doc(leaf->left->content, turn);
 		if (leaf->right)
-			parse_here_doc(leaf->right)
+			parse_here_doc(leaf->right, turn + 1)
 	}
 	else if (leaf->type == CMD)
 		check_here_doc(leaf->content);
 }
 
-void	check_here_doc(t_line *sub)
+void	check_here_doc(t_line *sub, int turn, int *fd_arr)
 {
 	t_block	*buff;
 
@@ -33,24 +56,38 @@ void	check_here_doc(t_line *sub)
 	{
 		//Ou on check les delimiter et on envoie pas le next a choix
 		if (buff->token == HERE_DOC)
-			create_and_fill(buff->next->word)
+			create_and_fill(buff->next->word, turn, fd_arr)
 		buff = buff->next;
 	}
 }
-//Malloc un tableau de fd pour chaque line avec le nombre de sub_line
 
-void	create_and_fill(char *delim)
+
+
+void	create_and_fill(char *delim, int turn, int *fd_arr)
 {
-	int	fd;
+	char	*new_delim;
 
 	join pour le name // checker que il soit pas deja present peut etre sinon modifier son nom jsp / 1 2 3 etc en function du block
-	fd = open(".tmp_here_doc", RDW_ONLY | O_CREATE | O_TRUNC, 0600);
-	if (fd == -1)
+	fd_arr[turn] = open(".tmp_here_doc", RDW_ONLY | O_CREATE | O_TRUNC, 0600);
+	if (fd_arr[turn] == -1)
 	{
 		perror("open crash");
 		return ;
 		//ou free_exit();
 	}
-	gnl qui write dans ce fd;
-	//gnl ou readline pour 
+	if (delim[0] == '\"')
+		new_delim = ft_strtrim(delim, '\"');
+	else
+		new_delim = delim;
+	char	*line;
+	line = get_next_line(0);
+	while (line && ft_strncmp(line, new_delim, ft_strlen(new_delim)) != 0)
+	{
+		if (line)
+			write(fd_arr[turn], line, ft_strlen(line));
+		line = get_next_line(0);
+	}
+	if (line)
+		write(fd_arr[turn], line, ft_strlen(line));
+	write(fd_arr[turn], 0, 1);
 }
