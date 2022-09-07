ba87ac509636fa776c18dc7c2fd85460697884ce
Maha BELRHAZI <mbelrhaz@e2r2p4.clusters.42paris.fr>
2022-09-07 18:44:33 +0200

    clean

--- obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: src/main.c includes/minishell.h libft/libft.h \
- includes/global.h includes/lexing_parsing.h includes/struct.h \
- includes/enum.h includes/exec.h includes/utils.h includes/expand.h
