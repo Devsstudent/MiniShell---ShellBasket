e3a22c7af44a508158468f956eb9cc52aa754b8d
mac <devsstudent777@gmail.com>
2022-09-15 09:51:23 +0200

    normed a bit

--- src/redir_ambiguous.c
+++ src/redir_ambiguous.c
@@ -22,7 +22,7 @@ void	ambiguous_case(t_bool type, t_info *exec_in)
 		exec_in->open_fd = -2;
 	write(2, msg, ft_strlen(msg));
 }
-//Empty ou contient des whitespace
+
 t_bool	check_ambiguous(char *word, t_info *exec_in, t_bool type)
 {
 	int		i;
@@ -40,7 +40,8 @@ t_bool	check_ambiguous(char *word, t_info *exec_in, t_bool type)
 	while (word[++i])
 	{
 		check_quote_redir(&d_quote, &quote, word[i]);
-		if (i > 0 && !is_white_space(word[i - 1]) && is_white_space(word[i]) && !quote && !d_quote)
+		if (i > 0 && !is_white_space(word[i - 1]) && is_white_space(word[i])
+			&& !quote && !d_quote)
 		{
 			ambiguous_case(type, exec_in);
 			return (TRUE);
