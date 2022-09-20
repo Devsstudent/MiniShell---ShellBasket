baa40808211d1d17666d6417e48d490a8e5c8967
Maha BELRHAZI <mbelrhaz@e1r4p1.clusters.42paris.fr>
2022-09-20 15:14:33 +0200

    final_out seems to be working for pipes and && and ||

--- bonus_dir/obj/parsing/here_doc.d
+++
@@ -1,4 +0,0 @@
-obj/parsing/here_doc.o: src/parsing/here_doc.c includes/minishell.h \
- ../libft/libft.h includes/global.h includes/struct.h includes/enum.h \
- includes/exec.h includes/lexing_parsing.h includes/utils.h \
- includes/expand.h
