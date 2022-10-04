e5320d789b6e9e75e834f6438cd4ca9cf47a36da
Devsstudent <devsstudent777@gmail.com>
2022-10-04 16:37:26 +0200

    save

--- shelltester/tester.sh
+++ shelltester/tester.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
 
-bash test > out
-(cd .. && make)
-../minishell
-echo test
+minishell_path=../
+red="\033[0;31m"
+green="\033[0;32m"
+reset="\033[0;39m"
+test_li=$(ls ./test/our_test/*)
+i=0
+#LOOP FILL SHELL OUTPUT (1 time)
+for file in $test_li
+do
+	cat $file | bash 2>&- > expected_output/qwe_$i
+	i=$((i + 1))
+done
+
+i=0
+#LOOP FILL MISHELL OUTPUT
+for file in $test_li
+do
+	cat $file | bash 2>&- > minishell_output/qwe_$i
+	i=$((i + 1))
+done
+
+i=0
+#LOOP DIFF
+for file in $(ls ./expected_output/*)
+do
+	DIFF=$(diff $file ./minishell_output/qwe_$i)
+	printf "$DIFF"
+	if [ $i -gt 25 ]
+	then
+		break
+	fi
+	if [ "$DIFF" ]
+	then
+		printf "$i: $red KO $reset\n"
+		cat $file
+	else
+		printf "\n$i: $green OK\n"
+	fi
+	printf "$reset"
+	i=$((i + 1))
+done
