26bdd899f90ad69b1b4b0afca9df27aa1ad9b077
Devsstudent <devsstudent777@gmail.com>
2022-10-12 16:12:46 +0200

    started norm bonus_part OUFF :'(

--- bonus_dir/src/exec/exec_bonus.c
+++ bonus_dir/src/exec/exec_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
-/*                                                                            */ 
+/*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   exec_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 15:14:15 by odessein          #+#    #+#             */
-/*   Updated: 2022/10/10 22:56:03 by mbelrhaz         ###   ########.fr       */
+/*   Created: 2022/10/12 15:44:51 by odessein          #+#    #+#             */
+/*   Updated: 2022/10/12 16:11:37 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -16,7 +16,6 @@ void	exec_cmd(t_info *exec_info, t_leaf *leaf, t_dict *env, t_leaf *prev)
 	expand(leaf->content, env);
 	wildcard(leaf->content);
 	exec_info->argv = get_cmd_arg(leaf->content);
-	//if (exec_info->open_fd != -2)
 	exec(exec_info, leaf, env, prev);
 }
 
@@ -32,11 +31,13 @@ static void	leaf_type_or(t_leaf *leaf, t_info *exec_in, t_dict *env)
 		exec_tree(leaf->right, exec_in, env, leaf);
 	else if (g_exit_status != 0)
 		exec_tree(leaf->right, exec_in, env, leaf);
-	else if (g_exit_status == 0 && leaf->right->parentheses != leaf->parentheses)
+	else if (g_exit_status == 0
+		&& leaf->right->parentheses != leaf->parentheses)
 		return ;
 }
 
-static void	leaf_type_and(t_leaf *leaf, t_info *exec_in, t_dict *env, t_leaf *prev)
+static void	leaf_type_and(t_leaf *leaf, t_info *exec_in, t_dict *env,
+		t_leaf *prev)
 {
 	if (prev && prev->type == OR_L && g_exit_status != 0)
 	{
@@ -55,22 +56,53 @@ static void	leaf_type_and(t_leaf *leaf, t_info *exec_in, t_dict *env, t_leaf *pr
 		exec_tree(leaf->right, exec_in, env, leaf);
 }
 
-static void	leaf_type_cmd_pipe(t_leaf *leaf, t_info *exec_in, t_dict *env, t_leaf *prev)
+void	leaf_type_cmd(t_leaf *leaf, t_info *exec_in, t_dict *env, t_leaf *prev)
+{
+	if (!prev)
+		exec_in->end = TRUE;
+	exec_cmd(exec_in, leaf, env, prev);
+}
+
+t_bool	parentheses_pipe(t_leaf *leaf, t_info *exec_in)
 {
 	int	pipe_fd[2];
 
+	if (leaf->left->parentheses > leaf->parentheses)
+	{
+		if (pipe(pipe_fd) == -1)
+			return (perror_false("Error creating pipe in pipe_exec"));
+		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+			return (perror_false("Error dup in exec_bonus"));
+		exec_in->pipe_fd[1] = pipe_fd[1];
+		exec_in->pipe_fd[0] = pipe_fd[0];
+	}
+	exec_in->pipe = TRUE;
+	return (TRUE);
+}
+
+t_bool	exec_left_right_pipe(t_leaf *leaf, t_info *exec_in, t_dict *env)
+{
+		exec_in->left = TRUE;
+		exec_in->right = FALSE;
+		exec_tree(leaf->left, exec_in, env, leaf);
+		exec_in->right = TRUE;
+		exec_in->left = FALSE;
+		if (leaf->left->parentheses > leaf->parentheses)
+			if (dup2(exec_in->stdou, STDOUT_FILENO) == -1)
+				return (perror_false("back to stdout"));
+		if ((leaf->head && leaf->right->type != PIPE_L && exec_in->right))
+			exec_in->end = TRUE;
+		exec_tree(leaf->right, exec_in, env, leaf);
+		return (TRUE);
+}
+
+static void	leaf_type_pipe(t_leaf *leaf, t_info *exec_in,
+				t_dict *env, t_leaf *prev)
+{
 	if (leaf->type == PIPE_L)
 	{
-		if (leaf->left->parentheses > leaf->parentheses)
-		{
-			if (pipe(pipe_fd) == -1)
-				return (perror("Error creating pipe in pipe_exec"));
-			if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-				return (perror("Error dup in exec_bonus"));
-			exec_in->pipe_fd[1] = pipe_fd[1];
-			exec_in->pipe_fd[0] = pipe_fd[0];
-		}
-		exec_in->pipe = TRUE;
+		if (!parentheses_pipe(leaf, exec_in))
+			return ;
 		if (prev && prev->type == PIPE_L)
 		{
 			if (prev->head && exec_in->right)
@@ -82,24 +114,49 @@ static void	leaf_type_cmd_pipe(t_leaf *leaf, t_info *exec_in, t_dict *env, t_lea
 			leaf->head = TRUE;
 			exec_in->prev_pipe = FALSE;
 		}
-		exec_in->left = TRUE;
-		exec_in->right = FALSE;
-		exec_tree(leaf->left, exec_in, env, leaf);
-		exec_in->right = TRUE;
-		exec_in->left = FALSE;
-		if (leaf->left->parentheses > leaf->parentheses)
-			if (dup2(exec_in->stdou, STDOUT_FILENO) == -1)
-				return (perror("back to stdout"));
-		if ((leaf->head && leaf->right->type != PIPE_L && exec_in->right))
-			exec_in->end = TRUE;
-		exec_tree(leaf->right, exec_in, env, leaf);
+		if (!exec_left_right_pipe(leaf, exec_in, env))
+			return ;
 	}
-	else
+}
+
+t_bool	leaf_red_in(t_leaf *leaf, t_info *exec_in, t_dict *env)
+{
+	if (leaf->type == RED_IN_L)
 	{
-		if (!prev)
-			exec_in->end = TRUE;
-		exec_cmd(exec_in, leaf, env, prev);
+		exec_in->open_fd = open(leaf->right->content->head->word, O_RDONLY);
+		if (exec_in->open_fd < 0)
+			return (perror_false("redir open fail"));
+		if (leaf->left->parentheses > leaf->parentheses)
+			if (dup2(exec_in->open_fd, STDIN_FILENO) == -1)
+				return (perror_false("back to stdout"));
+		exec_tree(leaf->left, exec_in, env, leaf);
 	}
+	return (TRUE);
+}
+
+t_bool	leaf_red_out(t_leaf *leaf, t_info *exec_in, t_dict *env)
+{
+	if (leaf->type == RED_OUT_TRUNC_L)
+	{
+		exec_in->out_fd = open(leaf->right->content->head->word, O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (exec_in->out_fd < 0)
+			return (perror_false("redir out fail"));
+		if (leaf->left->parentheses > leaf->parentheses)
+			if (dup2(exec_in->out_fd, STDOUT_FILENO) == -1)
+				return (perror_false("back to stdout"));
+		exec_tree(leaf->left, exec_in, env, leaf);
+	}
+	if (leaf->type == RED_OUT_APPEND_L)
+	{
+		exec_in->out_fd = open(leaf->right->content->head->word, O_CREAT | O_RDWR | O_APPEND, 0644);
+		if (exec_in->out_fd < 0)
+			return (perror_false("redir out fail"));
+		if (leaf->left->parentheses > leaf->parentheses)
+			if (dup2(exec_in->out_fd, STDOUT_FILENO) == -1)
+				return (perror_false("back to stdout"));
+		exec_tree(leaf->left, exec_in, env, leaf);
+	}
+	return (TRUE);
 }
 
 void	exec_tree(t_leaf *leaf, t_info *exec_in, t_dict *env, t_leaf *prev)
@@ -110,40 +167,16 @@ void	exec_tree(t_leaf *leaf, t_info *exec_in, t_dict *env, t_leaf *prev)
 		exec_in->fork = FALSE;
 		return ;
 	}
-	else if (leaf->type == PIPE_L || leaf->type == CMD)
-		leaf_type_cmd_pipe(leaf, exec_in, env, prev);
+	else if (leaf->type == PIPE_L)
+		leaf_type_pipe(leaf, exec_in, env, prev);
 	else if (leaf->type == OR_L)
 		leaf_type_or(leaf, exec_in, env);
+	else if (leaf->type == CMD)
+		leaf_type_cmd(leaf, exec_in, env, prev);
 	else if (leaf->type == AND_L)
 		leaf_type_and(leaf, exec_in, env, prev);
-	else if (leaf->type == RED_IN_L)
-	{
-		exec_in->open_fd = open(leaf->right->content->head->word, O_RDONLY);
-		if (exec_in->open_fd < 0)
-			return (perror("redir open fail"));
-		if (leaf->left->parentheses > leaf->parentheses)
-			if (dup2(exec_in->open_fd, STDIN_FILENO) == -1)
-				return (perror("back to stdout"));
-		exec_tree(leaf->left, exec_in, env, leaf);
-	}
-	else if (leaf->type == RED_OUT_TRUNC_L)
-	{
-		exec_in->out_fd = open(leaf->right->content->head->word, O_CREAT | O_RDWR | O_TRUNC, 0644);
-		if (exec_in->out_fd < 0)
-			return (perror("redir out fail"));
-		if (leaf->left->parentheses > leaf->parentheses)
-			if (dup2(exec_in->out_fd, STDOUT_FILENO) == -1)
-				return (perror("back to stdout"));
-		exec_tree(leaf->left, exec_in, env, leaf);
-	}
-	else if (leaf->type == RED_OUT_APPEND_L)
-	{
-		exec_in->out_fd = open(leaf->right->content->head->word, O_CREAT | O_RDWR | O_APPEND, 0644);
-		if (exec_in->out_fd < 0)
-			return (perror("redir out fail"));
-		if (leaf->left->parentheses > leaf->parentheses)
-			if (dup2(exec_in->out_fd, STDOUT_FILENO) == -1)
-				return (perror("back to stdout"));
-		exec_tree(leaf->left, exec_in, env, leaf);
-	}
+	else if (!leaf_red_in(leaf, exec_in, env))
+		return ;
+	else if (!leaf_red_out(leaf, exec_in, env))
+		return ;
 }
