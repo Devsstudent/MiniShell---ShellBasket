1291a4ad441af78b0a9a213297fbbef815f7dea1
Devsstudent <devsstudent777@gmail.com>
2022-10-19 20:15:42 +0200

    yay

--- shelltester/tester.sh
+++ shelltester/tester.sh
@@ -13,6 +13,15 @@ red="\033[0;31m"
 green="\033[0;32m"
 reset="\033[0;39m"
 test_li=$(ls ./test_bonus/*)
+cyan="\033[0;36m"
+#Setup a directory with a lot a testfile inside like mine and give the dirpath of your choice behind
+printf "$cyan     _______. __    __   _______  __       __      .___________. _______     _______.___________. _______ .______      \n"
+printf "    /       ||  |  |  | |   ____||  |     |  |     |           ||   ____|   /       |           ||   ____||   _  \     \n"
+printf "   |   (----\`|  |__|  | |  |__   |  |     |  |     \`---|  |----\`|  |__     |   (----\`---|  |----\`|  |__   |  |_)  |    \n"
+printf "    \   \    |   __   | |   __|  |  |     |  |         |  |     |   __|     \   \       |  |     |   __|  |      /     \n"
+printf ".----)   |   |  |  |  | |  |____ |  \`----.|  \`----.    |  |     |  |____.----)   |      |  |     |  |____ |  |\  \----.\n"
+printf "|_______/    |__|  |__| |_______||_______||_______|    |__|     |_______|_______/       |__|     |_______|| _| \`._____|"
+printf "$reset\n\n"
 
 printf "path : $minishell_dir_path\n"
 (cd $minishell_dir_path && make -s)
